c5b730dbe5db6e721d0c1bde17e19933f537946d
vincent.zheng <vincent.zheng@zoom.us>
2019-05-13 09:32:29 +0800

    更新帮助信息

--- message.cpp
+++ message.cpp
@@ -33,14 +33,14 @@ void Msg::showHelpMsg()
 {
     cout << "usage:[help] [h] [ls] [ls <id list>] [add] [quit]" << endl;
     cout << "      [exit] [load] [toogle mask] [reset] [saveas <filename>]" << endl << endl;
-    cout << "  help, h      show help messages." << endl;
-    cout << "  add          add a password record." << endl;
-    cout << "  load         load records form a file." << endl;
-    cout << "  toogle mask  toogle show password as \"******\" or normal." << endl;
-    cout << "  exit, quit   exit the program." << endl;
-    cout << "  reset        reset user name and password." << endl;
-    cout << "  saveas xxx   save records to file xxx." << endl;
-    cout << "  ls           list all records." << endl;
-    cout << "  ls id1 id2   list id1, id2 records." << endl;
+    cout << "  help, h        show help messages." << endl;
+    cout << "  add            add a password record." << endl;
+    cout << "  load           load records form a file." << endl;
+    cout << "  toogle mask    toogle show password as \"******\" or normal." << endl;
+    cout << "  exit, quit, q  exit the program." << endl;
+    cout << "  reset          reset user name and password." << endl;
+    cout << "  saveas xxx     save records to file xxx." << endl;
+    cout << "  ls             list all records." << endl;
+    cout << "  ls id1 id2     list id1, id2 records." << endl;
     cout << endl;
 }
