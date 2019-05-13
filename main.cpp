ae7baec4c45a93677e9d6572a5f105c857bb57e5
vincent.zheng <vincent.zheng@zoom.us>
2019-05-13 09:44:20 +0800

    删除无用注释

--- main.cpp
+++ main.cpp
@@ -8,20 +8,5 @@ int main()
     mgr.init();
     mgr.start();
     mgr.exit();
-    // DB test;
-    // PasswordList it;
-    // test.attachDB();
-    // test.createTable();
-    // test.readToList(it);
-    // it.display();
-    // cout << "---------" << endl;
-    // Passworditem it;
-    // test.insertRecord(it);
-    // string test = "    hello wordl    here is a test string    ";
-    // vector<string> out;
-    // PasswordList::stringSplit(test, out, ' ');
-    // for (int i = 0; i < out.size(); i++) {
-    //     cout << out[i] << "-";
-    // }
     return 0;
 }
